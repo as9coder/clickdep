5e227b7c0482bc3d8c76b04bcbae909b8d1e3023
as9coder <aanshsoodofficial102@gmail.com>
2026-02-27 19:41:02 +0530

    chore: remove test files

--- test-vm7.js
+++
@@ -1,5 +0,0 @@
-const vm = require('vm');
-const sandbox = { f: () => { } };
-sandbox.console = console;
-vm.createContext(sandbox);
-vm.runInContext('try{ console.log("Result:", f.constructor("return process")() !== undefined) } catch(e) { console.log("Err:", e.message) }', sandbox);
--- test-vm8.js
+++
@@ -1,5 +0,0 @@
-const vm = require('vm');
-const sandbox = { request: {} };
-sandbox.console = console;
-vm.createContext(sandbox);
-vm.runInContext('try{ console.log("Result:", request.constructor.constructor("return process")() !== undefined) } catch(e) { console.log("Err:", e.message) }', sandbox);
